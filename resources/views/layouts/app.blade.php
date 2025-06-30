b954b25e15a0f3ebe9cb918046066085741ed515
kazumi4646 <alfiardiansyah464@gmail.com>
2025-07-01 03:39:52 +0700

    change layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,20 +4,22 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
         <title>{{ config('app.name', 'Laravel') }}</title>
-
+        
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        
+        <!-- CSS - Hardcode sementara -->
+        <link rel="stylesheet" href="{{ asset('build/assets/css-CVUPaf4i.css') }}">
+        
+        <!-- Atau coba absolute path jika asset() tidak work -->
+        <!-- <link rel="stylesheet" href="/build/assets/css-CVUPaf4i.css"> -->
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
-
+            
             <!-- Page Heading -->
             @isset($header)
                 <header class="bg-white shadow">
@@ -26,11 +28,14 @@
                     </div>
                 </header>
             @endisset
-
+            
             <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
         </div>
+        
+        <!-- JS - Hardcode sementara -->
+        <script src="{{ asset('build/assets/app-DabVqtOm.js') }}"></script>
     </body>
 </html>
