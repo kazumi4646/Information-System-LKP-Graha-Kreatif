ba315a4a566a9ae0dfbd9aa1061ee28c43762de5
kazumi4646 <alfiardiansyah464@gmail.com>
2025-07-01 12:14:39 +0700

    change laYOUT AGAIN

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('build/assets/css-CVUPaf4i.css') }}">
     <link rel="stylesheet" href="/build/assets/css-CVUPaf4i.css">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script> 
