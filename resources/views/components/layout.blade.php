02fa53b1ec25ccd7207b71f433926d9078ec9b53
kazumi4646 <alfiardiansyah464@gmail.com>
2025-07-01 11:56:54 +0700

    change laYOUT AGAIN

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.css'])
+    <link rel="stylesheet" href="{{ asset('build/assets/css-CVUPaf4i.css') }}">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script> 
 
-    <title>test</title>
+    <title>LKP GRAHA KREATIF</title>
 </head>
   <body class="h-full bg-linear-to-t from-sky-100 to-indigo-200">
 
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+       <link rel="stylesheet" href="{{ asset('build/assets/css-CVUPaf4i.css') }}">
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <x-navbar></x-navbar>
